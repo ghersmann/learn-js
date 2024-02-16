4e1be04a2f82e8f7f5d95ecaf9ff856fc8a6b82f
ghersmann <gustav@gustavhersmann.com>
2024-02-16 08:05:42 +0100

    Task 3: First Functions

---
+++ 03-your-first-functions/script.js
@@ -0,0 +1,49 @@
+// 03-your-first-functions
+
+// TODO: implement welcomeMsg function
+
+function welcomeMsg(userName) {
+  return "Welcome" + userName;
+}
+
+console.log(welcomeMsg(" Jan!"));
+// Welcome Jane!
+
+console.log(welcomeMsg(" Ove!"));
+/* // Welcome Marc!
+
+
+
+// TODO: implement calcGrossPrice function */
+
+function calcGrossPrice(price, tax) {
+  return price * tax + price;
+}
+
+console.log(calcGrossPrice(20, 0.19));
+// result should be 23.8
+
+console.log(calcGrossPrice(40, 0.16));
+/* // result should be 46.4
+
+
+
+// TODO: Implement the addPositive function */
+
+function addPositive(a, b) {
+  if (a < 0) {
+    return a * -1 + b * -1;
+  } else if (b < 0) {
+    return a + b * -1;
+  }
+  return a + b;
+}
+
+console.log(addPositive(2, 3));
+// result1 should be 5
+
+console.log(addPositive(3, -5));
+// result2 should be 8
+
+console.log(addPositive(-1, -8));
+// result2 should be 9
