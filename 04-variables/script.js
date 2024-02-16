9f47f75338a66adc417844363e84804172aa0920
ghersmann <gustav@gustavhersmann.com>
2024-02-16 08:06:25 +0100

    Task 4: Variables

---
+++ 04-variables/script.js
@@ -0,0 +1,37 @@
+// 04-variables
+
+let userName = "Brad";
+userName = "Jenna";
+
+function getUserNameLength(userName) {
+  return userName.length;
+}
+
+console.log(getUserNameLength(userName) > 4);
+/*  ^______________ Should log true
+
+
+
+TODO: Implement the isString function */
+
+const isString = function (insertAnything) {
+  if (typeof insertAnything === "string") {
+    return true;
+  }
+  return false;
+};
+
+console.log(isString("Hello"));
+// result should be true
+
+console.log(isString(3));
+// result should be false
+
+console.log(isString(undefined));
+// result should be false
+
+console.log(isString(""));
+// result should be true
+
+console.log(isString("John" + "Doe"));
+// result should be true
